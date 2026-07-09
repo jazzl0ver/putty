bd0bddf9ff2a642d3af8feae61dee81cd06647b5
jazzl0ver <jazzl0ver@users.noreply.github.com>
2026-07-09 16:21:13 +0300

    releasing version 0.84k-r2

--- version.h
+++ version.h
@@ -8,6 +8,6 @@
  * default stuff used for local development runs of 'make'.
  */
 
-#define TEXTVER "0.84k-r1"
-#define SSHVER "-Release-0.84k-r1"
-#define BINARY_VERSION 0,84,1,0
+#define TEXTVER "0.84k-r2"
+#define SSHVER "-Release-0.84k-r2"
+#define BINARY_VERSION 0,84,2,0
